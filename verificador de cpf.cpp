c475df99449115c2a74c852fd6711aa7d3b37d5b
samuel bm <samuelbdemoura@gmail.com>
2026-03-17 22:09:16 -0300

    diminui a etrutura e validei a quantidade de digitos

--- verificador de cpf.cpp
+++ verificador de cpf.cpp
@@ -4,14 +4,16 @@
 //vetores(arrays)-cpf
  int main(){
  	char cpf[12];
- 	int resto,resto2,soma,soma2,i2,i,verify1,verify2;
+ 	int resto,resto2,soma,soma2,i,verify1,verify2;
  	printf("bem vindo ao validador de cpf,digite o cpf:\n");
-  scanf("%s", cpf);
+  scanf("%11s", cpf);
  	soma=0;
  	soma2=0;
  	for(i=0;i<=8;i++){
  		soma=soma+((cpf[i]-'0')*(10-i));
- 		
+	 }
+	 for(i=0;i<=9;i++){
+ 		soma2=soma2+((cpf[i]-'0')*(11-i));
 	 }
 	 resto=soma%11;
 	 if(resto==0 || resto==1){
@@ -19,9 +21,7 @@
 	 }else{
 	 	verify1=11-resto;
 	 }
-	 for(i2=0;i2<=9;i2++){
- 		soma2=soma2+((cpf[i2]-'0')*(11-i2));
-	 }
+	 
 	 resto2=soma2%11;
 	 if(resto2==0 || resto2==1){
 	 	verify2=0;
