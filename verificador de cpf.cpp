48fbb3945ab643f73e2c99b92e90fa0c11156062
SAMUEL B DE MOURA <samuelbdemoura@gmail.com>
2026-03-18 15:55:24 -0300

    Enhance CPF validation with input checks
    Added input validation for CPF length and characters.

--- verificador de cpf.cpp
+++ verificador de cpf.cpp
@@ -1,17 +1,31 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 //vetores verificar cpf
  int main(){
  	char cpf[12];
  	int resto,resto2,soma,soma2,i,verify1,verify2;
  	printf("bem vindo ao validador de cpf,digite o cpf:\n");
-  fgets(cpf,12, stdin);
- 	soma=0;
- 	soma2=0;
+ 	fgets(cpf,12,stdin);
+ 	cpf[strcspn(cpf,"\n")]='\0';  //o fgets le o \n,enter, junto com a entrada do usuário,o strcspn é um comando que pesquisa até encontrar o dado solicitado e retorna a posição dele,o ='\0' faz que ,a posição em que se encontre o \n agora seja o final do vetor
+  if(strlen(cpf)!=11){  // validação de tamanho da entrada, strlen(cpf) me retorna o tamanho do vetor
+  	printf(" --cpf invalido");
+  	return(1);
+  }
+  soma=0;
+	soma2=0;
+	for(i=0;i<10;i++){     
+		if(cpf[i]<'0' || cpf[i]>'9'){
+ 		 	printf(" -cpf invalido");
+			return(1);
+		  }//validação para excluir caracteres que nao sejam inteiros,na tabela asc,os numeros int seguem a sequencia de '0' a '9' 
+	}
+ 	
  	for(i=0;i<=8;i++){
  		soma=soma+((cpf[i]-'0')*(10-i));
+ 		
 	 }
+	
 	 for(i=0;i<=9;i++){
  		soma2=soma2+((cpf[i]-'0')*(11-i));
 	 }
@@ -29,9 +43,9 @@
 	 	verify2=11-resto2;
 	 }
 	 if((cpf[9]-'0')==verify1 && (cpf[10]-'0')==verify2 ){
-	 	printf("cpf valido");
+	 	printf("cpf valido %d %d", verify1, verify2);
 	 }else{
-	 	printf("cpf invalido");
+	 	printf("cpf invalido %d %d", verify1, resto);
 	 }
 	 return 0;
  }
