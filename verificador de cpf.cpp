3fbc5cdc5423fe688256661f1018ea0a46352635
SAMUEL B DE MOURA <samuelbdemoura@gmail.com>
2026-03-28 17:38:18 -0300

    Improve CPF validation structure with functions
    Refactor CPF validator to use separate functions for validation and digit verification.

--- verificador de cpf.cpp
+++ verificador de cpf.cpp
@@ -1,52 +1,72 @@
+//validador de cpf aprimorado com funÃ§Ãµes separadas
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-
-//vetores verificar cpf
- int main(){
- 	char cpf[12];
- 	int resto,resto2,soma,soma2,i,verify1,verify2;
- 	printf("bem vindo ao validador de cpf,digite o cpf:\n");
- 	fgets(cpf,12,stdin);
- 	cpf[strcspn(cpf,"\n")]='\0';  //o fgets le o \n,enter, junto com a entrada do usuário,o strcspn é um comando que pesquisa até encontrar o dado solicitado e retorna a posição dele,o ='\0' faz que ,a posição em que se encontre o \n agora seja o final do vetor
-  if(strlen(cpf)!=11){  // validação de tamanho da entrada, strlen(cpf) me retorna o tamanho do vetor
-  	printf(" --cpf invalido");
-  	return(1);
+#include <stdlib.h>
+int validacao(char cpf[]){
+    int i;
+    if(strlen(cpf)!=11){  //validar tamanho
+        return 1;
+    }
+    for(i=0;i<=10;i++){   //validaÃ§Ã£o de caracteres permitidos
+      if(cpf[i]<'0' || cpf[i]>'9'){
+        return 2;
+      }
+    }
+    return 0;
   }
-  soma=0;
-	soma2=0;
-	for(i=0;i<11;i++){     
-		if(cpf[i]<'0' || cpf[i]>'9'){
- 		 	printf(" -cpf invalido");
-			return(1);
-		  }//validação para excluir caracteres que nao sejam inteiros,na tabela asc,os numeros int seguem a sequencia de '0' a '9' 
-	}
- 	
- 	for(i=0;i<=8;i++){
- 		soma=soma+((cpf[i]-'0')*(10-i));
- 		
-	 }
-	
-	 for(i=0;i<=9;i++){
- 		soma2=soma2+((cpf[i]-'0')*(11-i));
-	 }
-	 resto=soma%11;
-	 if(resto==0 || resto==1){
-	 	verify1=0;
-	 }else{
-	 	verify1=11-resto;
-	 }
-	 
-	 resto2=soma2%11;
-	 if(resto2==0 || resto2==1){
-	 	verify2=0;
-	 }else{
-	 	verify2=11-resto2;
-	 }
-	 if((cpf[9]-'0')==verify1 && (cpf[10]-'0')==verify2 ){
-	 	printf("cpf valido %d %d", verify1, verify2);
-	 }else{
-	 	printf("cpf invalido %d %d", verify1, resto);
-	 }
-	 return 0;
- }
+  int verificador1(char cpf[]){
+      int soma1=0,dv1,i;
+      for(i=0;i<=8;i++){
+          soma1=soma1+(cpf[i]-'0')*(10-i);
+      }
+      if(soma1%11==0 || soma1%11==1){
+          dv1=0;
+      }else{
+          dv1=11-(soma1%11);
+      }
+     
+      return dv1;
+  }
+  int verificador2(char cpf[]){
+      int soma1=0,dv2,i;
+      for(i=0;i<=9;i++){
+          soma1=soma1+(cpf[i]-'0')*(11-i);
+      }
+      if(soma1%11<2){
+          dv2=0;
+      }else{
+          dv2=11-(soma1%11);
+      }
+     
+      return dv2;
+  }
+int main(){
+    int vali,c,digiv1,digiv2;
+    char cpf[12];
+    printf("bem vindo ao  validador de cpf\n");
+    do{
+    printf("digite seu cpf \n");
+    fgets(cpf,12,stdin);
+    cpf[strcspn(cpf,"\n")]='\0';
+    while((c=getchar()) !='\n' && c!=EOF);
+    vali=validacao(cpf);
+    if(vali==1){
+        printf("tamanho do cpf invalido, digite novamente\n");
+    }else if(vali==2){
+    	printf("cpf invalido,digite apenas numeros\n");
+	}else if(vali==0){
+	    digiv1=verificador1(cpf);
+	    digiv2=verificador2(cpf);
+	    
+        if(digiv1==(cpf[9]-'0') && digiv2==(cpf[10]-'0')){
+        puts("cpf valido");
+    }else{
+        puts("cpf invalido");
+        vali=3;
+    }
+    }
+    }while(vali!=0);
+    printf("%d,  %d",digiv1,digiv2);
+    
+  return 0;
+  
